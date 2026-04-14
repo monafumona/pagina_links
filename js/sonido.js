aba1791ffabeabb4f425eac6f831cfbc4f94cf60
Juan Giménez <jjabietar4@gmail.com>
2026-04-14 13:35:30 +0200

    Implement itemRandom function for random selection
    Added a new function to select random items.

--- js/sonido.js
+++ js/sonido.js
@@ -4,9 +4,43 @@ function reproducirSonido() {
         'resources/eructo2.mp3',
         'resources/eructo3.mp3'
     ];
-    
-    // Elige uno al azar
+
     const elegido = sonidos[Math.floor(Math.random() * sonidos.length)];
     const audio = new Audio(elegido);
     audio.play();
+}
+
+function itemRandom(item) {
+
+    if (item === "modelo") {
+        const imagenes = [
+            'resources/dino2.glb',
+            
+            'resources/dragon.glb',
+            'resources/huevo.glb'
+        ];
+
+        const elegido = imagenes[Math.floor(Math.random() * imagenes.length)];
+        return elegido;
+    }
+
+    if(item==="fondo"){
+        const imagenes = [
+            'img/fondo1.jpg',
+            'img/fondo2.jpg',
+            'img/fondo3.jpg',
+            'img/fondo4.jpg',
+            'img/fondo5.jpg',
+            'img/fondo6.jpg',
+            'img/fondo7.jpg',
+            'img/fondo8.jpg',
+            'img/fondo9.jpg',
+            'img/fondo10.jpg',
+            'img/fondo11.jpg',
+            'img/fondo12.jpg'
+        ];
+
+        const elegido = imagenes[Math.floor(Math.random() * imagenes.length)];
+        return elegido;
+    }
 }
